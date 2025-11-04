141ba5f227a4aa71b599ee242cdeee2709423335
prof_riemke <2960904+profriemke@users.noreply.github.com>
2025-11-04 17:38:44 +0100

    LV 04.11. (2)

--- 05-adonisjs/blog/start/routes.ts
+++ 05-adonisjs/blog/start/routes.ts
@@ -22,10 +22,58 @@ router.get('/post/create', async ({ view })=>{
     return view.render('pages/create')
 })
 
+router.get('/post/:id', async ({ view, params })=>{
+    console.log(params.id)
+    const post = await db.from('post')
+                         .select('*')
+                         .where('id', params.id)
+                         .first()
+    console.log(post)
+    return view.render('pages/post', {post})
+})
+
+
+
+
+
 router.post('/post/create', async ({ response, request })=>{
    
     const { title, teaser, text } = request.all()
-    // Wunder
 
+    if(!title || !teaser || !text){
+        return response.redirect('/post/create')
+    }
+
+    const result = await db.table('post').insert({
+        title: title,
+        teaser: teaser,
+        text: text,
+        date: new Date().toString(),
+        author: 'omm'
+    })
+    console.log(result)
     return response.redirect('/')
+})
+
+
+
+
+
+router.get('/a', async ({ session })=>{
+    session.put('nachricht', 'OMM!')
+    return 'gesetzt'
+})
+
+router.get('/b', async ({ session})=>{
+    return session.get('nachricht')
+})
+router.get('/counter', async ({ session })=>{
+    let counter = session.get('counter')
+    console.log(counter)
+    if(counter == undefined){
+        counter =0
+    }
+    counter = counter +1
+    session.put('counter', counter)
+    return counter
 })
